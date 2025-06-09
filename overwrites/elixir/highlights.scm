05d22964006869dda7920804b363f5a3a595855d
Leandro Pereira <leandro@leandro.io>
2025-06-09 19:06:08 -0400

    fix: elixir keyword

--- overwrites/elixir/highlights.scm
+++ overwrites/elixir/highlights.scm
@@ -13,3 +13,13 @@
         (boolean) @comment.doc
       ]))
   (#any-of? @comment.doc.__attribute__ "moduledoc" "typedoc" "doc"))
+
+; * definition keyword
+(call
+  target: (identifier) @keyword
+  (#any-of? @keyword "def" "defdelegate" "defexception" "defguard" "defguardp" "defimpl" "defmacro" "defmacrop" "defmodule" "defn" "defnp" "defoverridable" "defp" "defprotocol" "defstruct"))
+
+; * kernel or special forms keyword
+(call
+  target: (identifier) @keyword
+  (#any-of? @keyword "alias" "case" "cond" "for" "if" "import" "quote" "raise" "receive" "require" "reraise" "super" "throw" "try" "unless" "unquote" "unquote_splicing" "use" "with"))
